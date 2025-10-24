95e5e41c8eeebfd91b43d120ed0424d34239a04d
Artem <140013348+Archangelo4ek@users.noreply.github.com>
2025-10-24 18:56:05 +0300

    Fixed tasks

---
+++ tasks_l4_ajs/lab_4b.js
@@ -0,0 +1,21 @@
+const promise_1 = new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((data) => data.json())
+    .then((users) => { const resUsers = users.map(user => ({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        phone: user.phone
+    }))
+    resolve(console.log(resUsers)); })
+    .catch(error=>reject(console.log(error)));
+});
+
+const promise_2 = new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((data) => data.json())
+    .then((users) => {const resUsers = users.filter(user => user.completed == false)
+    resolve(console.log(resUsers));})
+    .catch(error=>reject(console.log(error)));
+});
